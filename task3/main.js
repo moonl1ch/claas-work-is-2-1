ea7649a3080f0f4710834e6dde8a930baca199c0
moonl1ch <niki.vershkov@gmail.com>
2024-02-05 15:11:05 +0300

    Адаптировал код

--- task3/main.js
+++ task3/main.js
@@ -1,61 +1,69 @@
-//let person = {
-  //  name: "Tom",
-    //age: 18,
-    //hobbies: ["Кушать", "Спать", "Просмотр кино", "Читать книги"],
-    //contacts: {
-      //  phone: [88005553255, 88905553255],
-       // email: ["tom@example.com", "tom2@example.com"],
-        //social: "www.github.com"
-//    }
-//}
-//
-//console.log(`Имя - ${person["name"]}. Возраст - ${person["age"]}`);
-//console.log(person["hobbies"][2]);
-//console.log(person["contacts"]["email"][1]);
-
 let college = [
-    { 
-        name: "Harry", 
-        age: 18,
-        group: "ИС-2-2",
+    {
+        name: "Harry",
+        age: 17,
+        group: "ИС-2-1",
         studID: 321232,
         typeLearning: "Очная",
-        task: ["Физ-ра","МДК-02-02","МДК-02-02","МДК-04-02",],
-        cash: 5500,
-        isStudent: true
-    },
-    {
-        name: "Ron", 
-        age: 19,
-        group: "ССА-3-2",
-        studID: 321232,
-        typeLearning: "Заочная",
-        task: ["ИЗО","БЖ","ПРАВО","МДК-04-02",],
+        task: ["Физ-ра", "МДК-02-02", "МДК-02-02", "МДК-04-01"],
         cash: 0,
         isStudent: true
     },
     {
-        name: "Tom", 
+        name: "Ron",
+        age: 19,
+        group: "ССА-3-2",
+        studID: 321236,
+        typeLearning: "Заочная",
+        task: ["ИЗО", "БЖ", "ПРАВО", "МДК-04-01"],
+        cash: 0,
+        isStudent: true
+    },
+    {
+        name: "Tom",
         age: 32,
         isKurator: true,
         kuratorGroup: "ССА-3-2",
         kafedra: "Инф.технологии",
-        predmet: ["МДК-02-01","МДК-02-2","МДК-09-02"],
-        isStudent: false
+        predmet: ["МДК-02-1","МДК-02-2","МДК-09.01","МДК-09-02"],
+        isStudent: false,
     },
     {
-        name: "bob", 
+        name: "Bob",
         age: 32,
         isKurator: true,
         kuratorGroup: "ССА-2-2",
         kafedra: "Инф.технологии",
-        predmet: ["МДК-02-01","МДК-02-2","МДК-09-02"],
-        isStudent: false
+        predmet: ["МДК-02-1","МДК-02-2","МДК-09.01","МДК-09-02"],
+        isStudent: false,
     }
 ];
 
-if (college[0]["isStudent"] === true){
-    console.log(`Студент ${college[0]["name"]}`)
-} else if (college[0]["isStudent"] === false){
-    console.log("Преподаватель")
+let student = college[2]; 
+
+if (student.isStudent) {
+  console.log(`Вы выбрали студента ${student.name}`);
+
+  if (student.age >= 18) {
+    console.log(`Студент ${student.name} совершеннолетний. Ему ${student.age}`);
+  } else {
+    console.log(`Студент ${student.name} несовершеннолетний. Ему ${student.age}`);
+  }
+
+  let stipendMessage;
+  if (student.cash >= 4500) {
+    stipendMessage = `получает повышенную стипендию (${student.cash} руб.)`;
+  } else if (student.cash > 0) {
+    stipendMessage = `получает обычную стипендию (${student.cash} руб.)`;
+  } else {
+    stipendMessage = `не получает стипендию`;
+  }
+
+  console.log(`Студент ${student.name} ${stipendMessage}`);
+  console.log(`Номер студенческого ${student.studID}`);
+} else if (student.isKurator) {
+  console.log(`Вы выбрали преподавателя ${student.name}`);
+  console.log(`Преподаватель ${student.name} куратор группы ${student.kuratorGroup}`);
+  console.log(`Кафедра ${student.kafedra}`)
+  console.log(`Ведет предметы ${student.predmet}`)
 }
