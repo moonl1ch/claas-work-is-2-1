14589d8b57051c15d093839168e909c08474e3c5
moonl1ch <niki.vershkov@gmail.com>
2024-02-05 13:52:13 +0300

    Выполнили работу с объектами

--- task3/main.js
+++ task3/main.js
@@ -1,23 +1,14 @@
-// let students = ["Андрей", "Иван", "Стас"];
-// students [50] = "Индекс 50"
-// console.log(students);
-
-
-// let college = [["Андрей", "Иван", "Стас"], [205,111,304]];
-// let kab  = college [1] [1]
-// console.log(kab)
-
-
-let number = [ 1,2,3,4 ];
-
-const coutLenght = 7;
-
-if ( number.length === coutLenght ) {
-    console.log("ok");
-} else if ( number.length < coutLenght ) {
-    let result = coutLenght - number.length;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно увеличить до ${coutLenght}. Прибавьте к массиву значение ${result}`);
-} else if ( number.length > coutLenght ) {
-    result = number.length - coutLenght;
-    console.log(`Длина вашего массива: ${number.length}. Длину массива нужно уменьшить до ${coutLenght}. Удалите из массива значение ${result}`)
+let person = {
+    name: "Tom",
+    age: 18,
+    hobbies: ["Кушать", "Спать", "Просмотр кино", "Читать книги"],
+    contacts: {
+        phone: [88005553255, 88905553255],
+        email: ["tom@example.com", "tom2@example.com"],
+        social: "www.github.com"
+    }
 }
+
+console.log(`Имя - ${person["name"]}. Возраст - ${person["age"]}`);
+console.log(person["hobbies"][2]);
+console.log(person["contacts"]["email"][1]);
